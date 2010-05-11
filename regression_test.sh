052700597f02c06a96e20b34fb130a2c99ea0fd1
Joachim B Haga <jobh@simula.no>
2010-05-11 09:35:04 +0200

    Expand regression test: use both python2.6 and python3

--- regression_test.sh
+++ regression_test.sh
@@ -4,9 +4,11 @@ for file in $(git ls-files | grep '\.texp$'); do
     dir=${file%/*}
     target=${file##*/}
     pushd $dir >/dev/null
-    rm -f $target
-    echo -n "$dir> "
-    make $target
-    PAGER= git diff --exit-code $target || { echo "! $file failed"; exit 1; }
+    for PYTHON in python3 python2.6; do
+        rm -f $target
+        echo -n "$PYTHON $dir\$ "
+        make PYTHON=$python $target
+        PAGER= git diff --exit-code $target || { echo "! $file failed with $PYTHON"; exit 1; }
+    done
     popd >/dev/null
 done
